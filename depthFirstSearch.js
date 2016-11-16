b1ea088c0f9d885f9b9d03403a37aba0e4b5f2c6
Bryan Choe <bryan.choe88@gmail.com>
2016-11-15 16:33:34 -1000

    depth first search sort-of working

--- depthFirstSearch.js
+++ depthFirstSearch.js
@@ -2,8 +2,26 @@
 
 const Node = require('./graphGenerator');
 
+let stack = [];
+let node;
+let visited = [];
+
 const DFS = (start, searchFor) => {
 
+  stack.push(start);
+  //console.log('stack*******',stack);
+  while(stack.length > 0){
+    node = stack.pop();
+    console.log('node*******', node);
+  }
 }
 
 module.exports = DFS;
+
+
+// stack.push(searchFor)
+// white s != empty
+// let start = searchfor.pop()
+// if (start != labeled as discovered)
+//   label search For as discovered
+//     for each edges/neighbors from v to w in start.adjacent edges/neighbors(searchFor) do stack.push w
